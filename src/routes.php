f2735eff579ab86ef54aa350accf2596a7974d5e
mathysfrc <mathysf41@gmail.com>
2023-04-21 11:29:17 +0200

    modifs

--- src/routes.php
+++ src/routes.php
@@ -13,5 +13,5 @@ return [
     'items/show' => ['ItemController', 'show', ['id']],
     'items/add' => ['ItemController', 'add',],
     'items/delete' => ['ItemController', 'delete',],
-    'partner/show' => ['PartnerController', 'index'],
+    'partner' => ['PartnerController', 'index'],
 ];
