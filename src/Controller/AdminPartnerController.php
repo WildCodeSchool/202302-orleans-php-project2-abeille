63f6c6ab6d9a591ca061ac65095c290c009c6bc8
mathysfrc <mathysf41@gmail.com>
2023-04-26 14:25:32 +0200

    modifs project

--- src/Controller/AdminPartnerController.php
+++ src/Controller/AdminPartnerController.php
@@ -22,36 +22,34 @@ class AdminPartnerController extends AbstractController
 
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $partner = array_map('trim', $_POST);
-        
 
-        if (empty($partner['name'])) {
-            $errors[] = 'Le champ nom est obligatoire';
+            if (empty($partner['name'])) {
+                $errors[] = 'Le champ nom est obligatoire';
+            }
+
+            $maxLength = 255;
+
+            if (mb_strlen($partner['name']) > $maxLength) {
+                $errors[] = 'Le champ nom doit faire moins de ' . $maxLength . ' caractères';
+            }
+
+            if (empty($partner['link'])) {
+                $errors[] = 'Le champ lien est obligatoire';
+            }
+
+            $maxLength = 255;
+
+            if (mb_strlen($partner['link']) > $maxLength) {
+                $errors[] = 'Le champ nom doit faire moins de ' . $maxLength . ' caractères';
+            }
+
+            if (empty($errors)) {
+                $partnerManager = new PartnerManager();
+                $partnerManager->insert($partner);
+                header('Location: /admin/partenaire');
+            }
         }
-
-        $maxLength = 255;
-
-        if (mb_strlen($partner['name']) > $maxLength) {
-            $errors[] = 'Le champ nom doit faire moins de ' . $maxLength . ' caractères';
-        }
-
-        if (empty($partner['link'])) {
-            $errors[] = 'Le champ lien est obligatoire';
-        }
-
-        $maxLength = 255;
-
-        if (mb_strlen($partner['link']) > $maxLength) {
-            $errors[] = 'Le champ nom doit faire moins de ' . $maxLength . ' caractères';
-        }
-
-        if (empty($errors)) {
-            $partnerManager = new PartnerManager();
-            $partnerManager->insert($partner);
-            header('Location: /admin/partenaire');
-        }
-    }
 
         return $this->twig->render('Admin/Partner/create.html.twig', ['errors' => $errors, 'partner' => $partner]);
-
     }
 }
