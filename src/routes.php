3ca119f373e2b374fd1006271a8d6753d23cf7a1
Abecker37 <72383676+Abecker37@users.noreply.github.com>
2023-04-26 15:59:15 +0200

    index event

--- src/routes.php
+++ src/routes.php
@@ -19,4 +19,5 @@ return [
     'admin/partenaire' => ['AdminPartnerController', 'index',],
     'admin/faq/ajouter' => ['AdminFaqController', 'add',],
     'admin/faq/index' => ['AdminFaqController', 'index',],
+    'admin/event/index' => ['AdminEventController', 'index',],
 ];
