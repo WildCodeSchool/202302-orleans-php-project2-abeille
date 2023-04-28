b907e8b1b655391776638eddf0dd12d956c0dfdd
mathysfrc <mathysf41@gmail.com>
2023-04-28 16:57:48 +0200

    modif

--- database.sql
+++ database.sql
@@ -80,11 +80,11 @@ CREATE TABLE `event` (
   `location` VARCHAR(150),
   `date` DATE NOT NULL);
 
-INSERT INTO event (name,description,location,date) VALUES('Visite de nos ruches','Nous allons visiter nos ruches et expliquer au différents visteur comme cela fonctionne.','Olivet Domaine de l‘Abeille Olivétaine.','2023-05-22');
+INSERT INTO event (name,description,location,date) VALUES('Visite de nos ruches','Nous organisons la visite nos ruches aux visiteurs afin qu\'ils en comprennent le fonctionnement.','Domaine de l‘Abeille Olivetaine, Olivet.','2023-05-22');
 
-INSERT INTO event (name,description,location,date) VALUES('Création de Ruches','Nous Allons créé des Ruches dans notre nouveau domaines','Olivet Domaine Jean Pernaud','2023-07-10');
+INSERT INTO event (name,description,location,date) VALUES('Création de Ruches','Création de Ruches sur notre nouveau domaine','Domaine Jean Pernaud, Olivet','2023-07-10');
 
-INSERT INTO event (name,description,location,date) VALUES('Découverte des abeilles','Nous allons faire découvrir les abeilles à nos visiteurs','Olivet Domaine de l’Abeille Olivetaine','2023-05-10');
+INSERT INTO event (name,description,location,date) VALUES('Découverte des abeilles','Nous allons faire découvrir les abeilles à nos visiteurs','Domaine de l’Abeille Olivetaine, Olivet','2023-05-10');
 
 CREATE TABLE `faq` (
   `id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
