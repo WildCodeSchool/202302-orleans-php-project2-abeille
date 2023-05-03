16d9267279d379fd4241145e7bc36968e8c3358c
mathysfrc <mathysf41@gmail.com>
2023-05-03 15:46:28 +0200

    modif contact page

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -6,6 +6,37 @@ class ContactController extends AbstractController
 {
     public function index(): string
     {
-        return $this->twig->render('Contact/contact.html.twig');
+        $errors = $this->validate($_POST);
+        return $this->twig->render('Contact/contact.html.twig', ['errors' => $errors]);
+    }
+
+    private function validate(array $contact)
+    {
+        $errors = [];
+
+        foreach ($contact as $key => $value) {
+            $contact[$key] = trim(($value));
+        }
+
+        if (empty($contact['user_firstname'])) {
+            $errors[] = 'Le prénom est obligatoire';
+        }
+        if (empty($contact['user_lastname'])) {
+            $errors[] = 'Le nom est obligatoire';
+        }
+
+        if (empty($contact['user_email'])) {
+            $errors[] = 'L\'email est obligatoire';
+        }
+        if (!filter_var($contact['user_email'], FILTER_VALIDATE_EMAIL)) {
+            $errors[] = "L'email est incorrect";
+        }
+        if (empty($contact['user_number'])) {
+            $errors[] = 'Le numéro de téléphone est obligatoire';
+        }
+        if (empty($contact['user_message'])) {
+            $errors[] = 'Le message est obligatoire';
+        }
+            return $errors;
     }
 }
