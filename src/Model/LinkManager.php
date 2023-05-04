5db0fc59c0c35309ef691121fbeb8ae149e0e301
herve <herveperel@gmail.com>
2023-05-04 12:27:38 +0200

    S04US11

--- src/Model/LinkManager.php
+++ src/Model/LinkManager.php
@@ -7,17 +7,4 @@ use PDO;
 class LinkManager extends AbstractManager
 {
     public const TABLE = 'link';
-    public function insert(array $faq): int
-    {
-        $statement = $this->pdo->prepare(
-            "INSERT INTO " . self::TABLE . " (`name`, `webAdress`)
-             VALUES (:name, :webAdress)"
-        );
-
-        $statement->bindValue(':name', $faq['name'], PDO::PARAM_STR);
-        $statement->bindValue(':webAdress', $faq['webAdress'], PDO::PARAM_STR);
-
-        $statement->execute();
-        return (int)$this->pdo->lastInsertId();
-    }
 }
